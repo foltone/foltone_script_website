ecdfbfb937f183c4764486b8a32b0e204df33a9d
foltone <77021937+foltone@users.noreply.github.com>
2023-02-12 19:51:02 +0100

    push v2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # foltone_script_website
 
 Voici un petit site vitrine.
+
+## Fabriqué avec
+
+* [SCSS](https://sass-lang.com/) - Préprocesseur css
+
+## License
+
+Ce projet est sous licence ``Licence Affero GPL (AGPL)`` - voir le fichier [LICENSE](LICENSE) pour plus d'informations
--- script/script.js
+++ script/script.js
@@ -1,43 +1,129 @@
-const chk = document.getElementById('chk');
-const checked = JSON.parse(localStorage.getItem("chk"));
-const loader = document.querySelector('.loader');
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    if (menu.style.display != "block") {
+        // Show menu
+        menu.style.display = "block";
+        // appearance animation
+        menu.style.opacity = 0;
+        menu.style.transform = "translateY(-10px)";
+        setTimeout(() => {
+            menu.style.opacity = 1;
+            menu.style.transform = "translateY(0)";
+        } , 100);
 
-chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark');
-});
-
-function save() {
-  localStorage.setItem("chk", chk.checked);
+    } else {
+        menu.style.display = "none";
+    }
 }
 
-document.getElementById("chk").checked = checked;
+document.querySelector("body").addEventListener("mousemove", eyeball);
 
-window.addEventListener("load", function() {
-  loader.classList.add('fondu-out');
-  if (checked) {
-    document.body.classList.toggle('dark');
-  }
-});
-
-function scriptTemplate(script) {
-  return `
-    <div class="grid-item">
-          <p>${script.name}</p>
-          <iframe src="https://www.youtube.com/embed/${script.vidéo}" frameborder="0"></iframe>
-          <div class="buttons">
-          <a href="${script.github}/archive/refs/heads/main.zip" target="_blank" class="bt-download"><i class="fa fa-download"></i>Download</a>
-          <a href="${script.github}" target="_blank" class="bt-repositorie"><i class="fa fa-github"></i>Repositorie</a>
-          </div>
-      </div>
-  `;
+function eyeball() {
+    const eye = document.querySelectorAll(".eyes");
+    eye.forEach(function (eye) {
+        let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+        let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rotate = radian * (180 / Math.PI) * -1 + 270;
+        eye.style.transform = "rotate(" + rotate + "deg)";
+    });
 }
 
-const requestURL = 'https://foltone.github.io/foltone_script_website/json/data.json';
-const request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-  const data = request.response
-  document.getElementById("grid-container").innerHTML = `${data.map(scriptTemplate).join("")}`;
+const data = [
+    {
+        name: "foltone ticket",
+        vidéo: "5WvTWMpKUcM",
+        github: "https://github.com/foltone/foltone_ticket",
+        cfx: "https://forum.cfx.re/t/free-standalone-ticket-system-rageui-fr-en-es/5009680"
+    },
+    {
+        name: "foltone ped meca",
+        vidéo: "-EGvdVIrkLk",
+        github: "https://github.com/foltone/foltone_ped_meca",
+        cfx: "https://forum.cfx.re/t/free-nui-automatic-ped-mechanic-fr-en-es/4994016"
+    },
+    {
+        name: "foltone drugs",
+        vidéo: "bW3_jxIvShQ",
+        github: "https://github.com/foltone/foltone_drugs"
+    },
+    {
+        name: "foltone bot",
+        vidéo: "bEadR2IkGYU",
+        github: "https://github.com/foltone/foltone_bot"
+    },
+    {
+        name: "foltone distributeur",
+        vidéo: "d_H2Se9NwGI",
+        github: "https://github.com/foltone/foltone_distributeur"
+    },
+    {
+        name: "foltone accessories",
+        vidéo: "YdjPtrOVNr8",
+        github: "https://github.com/foltone/foltone_accessories"
+    },
+    {
+        name: "foltone clothes",
+        vidéo: "YdjPtrOVNr8",
+        github: "https://github.com/foltone/foltone_clothes"
+    },
+    {
+        name: "foltone template",
+        vidéo: "J3D7ad_v1B0",
+        github: "https://github.com/foltone/foltone_template"
+    },
+    {
+        name: "foltone keys",
+        vidéo: "lZMfPSfP-qo",
+        github: "https://github.com/foltone/foltone_keys"
+    },
+    {
+        name: "foltone f5",
+        vidéo: "D16ODqkkInA",
+        github: "https://github.com/foltone/foltone_f5"
+    },
+    {
+        name: "foltone ambulance job",
+        vidéo: "gIH_8Y3PxqU",
+        github: "https://github.com/foltone/foltone_ambulance_job"
+    },
+    {
+        name: "foltone pack shops",
+        vidéo: "whDHVl6aW48",
+        github: "https://github.com/foltone/foltone_pack_shops"
+    },
+    {
+        name: "folltone zone safe",
+        vidéo: "R1hPkNakpkU",
+        github: "https://github.com/foltone/foltone_zone_safe"
+    }
+
+]
+
+const scripts = document.getElementById("grid-scripts");
+
+for (const item of data) {
+    const div = document.createElement("div");
+    div.classList.add("script");
+    if (item.cfx) {
+        div.innerHTML = `
+        <h1>${item.name}</h1>
+        <iframe src="https://www.youtube.com/embed/${item.vidéo}" frameborder="0">></iframe>
+        <div class="list-btn">
+            <a target="_blank" href="${item.github}" class="btn"><img src="img/fivem.png">Post</a>
+            <a target="_blank" href="${item.github}" class="btn"><img src="img/github.png">Repository</a>
+            <a target="_blank" href="${item.github}/archive/refs/heads/main.zip" class="btn"><img src="img/download.png">Download</a>
+        </div>
+        `;
+    } else {
+        div.innerHTML = `
+        <h1>${item.name}</h1>
+        <iframe src="https://www.youtube.com/embed/${item.vidéo}" frameborder="0">></iframe>
+        <div class="list-btn">
+            <a target="_blank" href="${item.github}" class="btn"><img src="img/github.png">Repository</a>
+            <a target="_blank" href="${item.github}/archive/refs/heads/main.zip" class="btn"><img src="img/download.png">Download</a>
+        </div>
+        `;
+    }
+    scripts.appendChild(div);
 }
